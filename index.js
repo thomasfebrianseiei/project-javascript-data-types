a2a39adc3c945fe76ca00f34c246c996ff799961
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2019-09-02 11:58:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log(`Hello World`);
+console.warn(`Hello Warning`);
+console.error(`Hello error`);
+console.info(`hello Alert`);
+
+// STRING
+console.log(`hello world`);
+
+const name = `Thomas Febrianto`;
+
+console.log(`Hello ${name}`);
+
+alert(`Hello ${name}`);
+
+// NUMBER
+console.log(0);
+console.log(100);
+console.log(3.14);
+
+const age = 52;
+
+console.log(`His age is ${age}`);
+
+alert(`His age is ${age}`);
+
+// OBJECT
+console.log({ name: "Alpha", age: 100, superPower: true });
+console.log({
+  name: "Alpha",
+  age: 100,
+  superPower: true
+});
+
+const hero = { name: "Alpha", age: 100, superPower: true };
+
+console.log(hero);
+console.log(hero["name"]);
+console.log(hero.age);
+console.log(hero.superPower);
+
+alert(hero);
+alert(hero["name"]);
+alert(hero.age);
+alert(hero.superPower);
+
+// ARRAY
+console.log([1, 2, 3])
+console.log([0, 1, 2, 3])
+console.log(['Alpha', 100, true])
+
+const person = ['Alpha', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person[0])
+alert(person[1])
+alert(person[2])
+
+// BOOLEAN
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = false
+
+console.log(`The lamp's toggle is ${toggle}`)
+
+alert(`The lamp's toggle is ${toggle}`)
